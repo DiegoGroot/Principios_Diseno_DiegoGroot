b3bbf2b0de60a7a400398a29133bec0877e6bf29
DiegoGroot <alejandrogrootc@gmail.com>
2026-03-19 21:08:33 -0600

    Add Flutter web build to static resources

--- demopg/src/test/java/com/postgres/demopg/DemopgApplicationTests.java
+++ demopg/src/test/java/com/postgres/demopg/DemopgApplicationTests.java
@@ -1,13 +1,14 @@
 package com.postgres.demopg;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class DemopgApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
